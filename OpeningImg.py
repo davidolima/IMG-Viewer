911423b2d001beee65c929f80e9886367484e877
David de Oliveira Lima <dipias2002@gmail.com>
2020-07-08 14:57:30 -0300

    When opening an image, It scales according to its aspect ratio to the screen. Added a BG color and an error screen.

--- OpeningImg.py
+++ OpeningImg.py
@@ -1,72 +1,102 @@
-import os, ctypes
-from PIL import Image
+import os
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPixmap, QFileOpenEvent, QIcon
-from PyQt5.QtWidgets import (QWidget, QLabel,QSizePolicy,QSizeGrip,QApplication, QFileDialog)
+from PyQt5.QtCore import QEvent, QSize
+from PyQt5.QtGui import QPixmap, QFileOpenEvent, QIcon, QResizeEvent
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import (QWidget, QLabel,QSizePolicy,QSizeGrip,QApplication, QFileDialog, QDesktopWidget, QMessageBox)
 
 class Ui_MAIN(object):
 
-    def setupUI(self, MAIN):
+    def init(self, MAIN):
+
+        #setting things up
         MAIN.setObjectName("MAIN")
-        MAIN.resize(500,500)
+        MAIN.setMinimumSize(500,500)
         scriptDir = os.path.dirname(os.path.realpath(__file__))
         MAIN.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'Icon.png'))
+        MAIN.setStyleSheet("background-color: gray;")
 
+        self.Frame = QtWidgets.QWidget(MAIN)
+        self.Frame.setSizeIncrement(QSize(30,30))
+        self.Img = QtWidgets.QLabel(self.Frame)
+        
+        sysRes = QApplication.desktop().screenGeometry()
+        self.sysWidth = sysRes.width()
+        self.sysHeight = sysRes.height()
 
+        MAIN.setMaximumSize(self.sysWidth,self.sysHeight)
+        self.Frame.setMaximumSize(self.sysWidth,self.sysHeight)
+        self.Img.setMaximumSize(self.sysWidth,self.sysHeight)
 
-        self.centralwidget = QtWidgets.QWidget(MAIN)
-        self.centralwidget.setObjectName("centralwidget")
-        self.centralwidget.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
-        self.Img = QtWidgets.QLabel(self.centralwidget)
-        self.Img.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
-
+        #Program running
         self.retranslateUi(MAIN)
         
-        user32 = ctypes.windll.user32
-        MAIN.setMaximumSize(user32.GetSystemMetrics(78),user32.GetSystemMetrics(79))
-        self.centralwidget.setMaximumSize(user32.GetSystemMetrics(78),user32.GetSystemMetrics(79))
-        self.Img.setMaximumSize(user32.GetSystemMetrics(78),user32.GetSystemMetrics(79))
-
-        self.file = QFileDialog.getOpenFileName()[0]
-        self.imgFile = QPixmap(self.file,'r')
+        try:
+            self.file = QFileDialog.getOpenFileName()[0]
+            self.imgFile = QPixmap(self.file,'r')
+            self.resizeWindow(MAIN)
+            self.centerWindow(MAIN)
         
-        imgDimensions = str(self.imgFile.size())[str(self.imgFile.size()).index("("):]
-        print(imgDimensions)
-        imgWidth = int(imgDimensions[:imgDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
-        imgHeight = int(imgDimensions[imgDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
-
-        fileName = self.file[self.file.rindex("/")+1:] + " ({}x{})".format(imgWidth,imgHeight)
-
-        MAIN.setWindowTitle(fileName)
-        print("\nImage Width:",imgWidth,"\nImage Height:",imgHeight)
-
-        mwDimensions = str(self.Img.maximumSize())[str(self.Img.maximumSize()).index("(")+1:]
-        mwWidth = int(mwDimensions[:mwDimensions.index(",")].replace(",","").replace(" ","").replace(" ","").replace(")",""))
-        mwHeight = int(mwDimensions[mwDimensions.index(","):].replace(",","").replace(" ","").replace(" ","").replace(")",""))
-
-        print("\nMaximum Width",mwWidth,"\nMaximum Height:",mwHeight)
-
-        if imgHeight > mwHeight:
-            self.imgFile = self.imgFile.scaled(self.imgFile.width(),mwHeight)
-        
-        if imgWidth > mwWidth:
-            self.imgFile = self.imgFile.scaled(mwWidth, self.imgFile.height())
-
-        self.Img.setPixmap(self.imgFile)
-        self.centralwidget.setGeometry(0,0,self.imgFile.width(),self.imgFile.height())
-        MAIN.resize(self.imgFile.width(),self.imgFile.height())
+        except:
+            self.error = QMessageBox(MAIN)
+            self.error.setWindowTitle("NOT AN IMAGE FILE!")
+            self.error.setText("Please select an image file and try again.")
+            self.error.show()
 
 
     def retranslateUi(self, MAIN):
         _translate = QtCore.QCoreApplication.translate
         MAIN.setWindowTitle(_translate("MAIN", "IMG Viewer"))
 
+    def centerWindow(self, MAIN):
+        rect = MAIN.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        rect.moveCenter(cp)
+        # MAIN.move(rect.topLeft())
+
+    def resizeWindow(self, MAIN):
+
+        imgDimensions = str(self.imgFile.size())[str(self.imgFile.size()).index("("):]
+        imgWidth = int(imgDimensions[:imgDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
+        imgHeight = int(imgDimensions[imgDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
+
+        fileName = self.file[self.file.rindex("/")+1:] + " ({}x{})".format(imgWidth,imgHeight)
+
+        print("\nImage Width:",imgWidth,"\nImage Height:",imgHeight)
+        print("\nMaximum Width:",self.sysWidth,"\nMaximum Height:",self.sysHeight)
+
+        if imgHeight > self.sysHeight:
+            self.imgFile = self.imgFile.scaled(imgWidth,self.sysHeight, QtCore.Qt.KeepAspectRatio)
+            imgDimensions = str(self.imgFile.size())[str(self.imgFile.size()).index("("):]
+            imgWidth = int(imgDimensions[:imgDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
+            imgHeight = int(imgDimensions[imgDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
+        
+        if imgWidth > self.sysWidth:
+            self.imgFile = self.imgFile.scaled(self.sysWidth, imgHeight, QtCore.Qt.KeepAspectRatio)
+            imgDimensions = str(self.imgFile.size())[str(self.imgFile.size()).index("("):]
+            imgWidth = int(imgDimensions[:imgDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
+            imgHeight = int(imgDimensions[imgDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
+    
+
+
+        MAIN.setMinimumSize(self.imgFile.width(),self.imgFile.height())
+        MAIN.showMaximized()
+        self.Img.setPixmap(self.imgFile)
+        rect = self.Frame.frameGeometry()
+        cp = MAIN.frameGeometry().center()
+        rect.moveCenter(cp)
+        self.Frame.move(rect.topLeft())
+
+        MAIN.setWindowTitle(fileName)
+        # MAIN.resize(self.sysWidth,self.sysHeight)
+        self.Frame.setGeometry(0,0,imgWidth,self.sysHeight)
+        self.Img.adjustSize()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MAIN = QtWidgets.QMainWindow()
     ui = Ui_MAIN()
-    ui.setupUI(MAIN)
+    ui.init(MAIN)
     MAIN.show()
     sys.exit(app.exec_())
