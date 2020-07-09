e5b40244ee0474d60ac771243a6e94421691991c
David de Oliveira Lima <dipias2002@gmail.com>
2020-07-09 01:44:23 -0300

    Images now keep themselves centered and can be moved..
    - Images now stays centered.
    - Gray background and dropshadow around the Image.
    - Fixed some bugs.

--- OpeningImg.py
+++ OpeningImg.py
@@ -1,14 +1,16 @@
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QEvent, QSize
-from PyQt5.QtGui import QPixmap, QFileOpenEvent, QIcon, QResizeEvent
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QPixmap, QFileOpenEvent, QIcon, QResizeEvent, QMouseEvent
 from PyQt5.QtGui import *
-from PyQt5.QtWidgets import (QWidget, QLabel,QSizePolicy,QSizeGrip,QApplication, QFileDialog, QDesktopWidget, QMessageBox)
+from PyQt5.QtWidgets import (QWidget, QLabel, QSizePolicy, QSizeGrip, QApplication, QFileDialog, QDesktopWidget, QMessageBox, QFrame)
 
-class Ui_MAIN(object):
+class MainWindow(QWidget):
 
     def init(self, MAIN):
-
+        super().__init__()
+        
         #setting things up
         MAIN.setObjectName("MAIN")
         MAIN.setMinimumSize(500,500)
@@ -17,8 +19,10 @@ class Ui_MAIN(object):
         MAIN.setStyleSheet("background-color: gray;")
 
         self.Frame = QtWidgets.QWidget(MAIN)
-        self.Frame.setSizeIncrement(QSize(30,30))
         self.Img = QtWidgets.QLabel(self.Frame)
+        # self.Img.setStyleSheet("background-color: red;")
+        self.Img.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        self.Img.setLineWidth(2)
         
         sysRes = QApplication.desktop().screenGeometry()
         self.sysWidth = sysRes.width()
@@ -35,7 +39,7 @@ class Ui_MAIN(object):
             self.file = QFileDialog.getOpenFileName()[0]
             self.imgFile = QPixmap(self.file,'r')
             self.resizeWindow(MAIN)
-            self.centerWindow(MAIN)
+            self.centerWindow()
         
         except:
             self.error = QMessageBox(MAIN)
@@ -44,18 +48,42 @@ class Ui_MAIN(object):
             self.error.show()
 
 
+    def resizeEvent(self, event):
+
+        self.Frame.setMinimumSize(event.size())
+        self.Frame.setMaximumSize(event.size())
+        self.centerWindow()
+
+        # self.Img.size().scaled(self.Frame.maximumSize(), QtCore.Qt.KeepAspectRatio)
+
+        # oldEventDimensions = str(event.oldSize())[str(event.oldSize()).index("("):]
+        # oldEventWidth = int(oldEventDimensions[:oldEventDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
+        # oldEventHeight = int(oldEventDimensions[oldEventDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
+
+        # newEventDimensions = str(event.size())[str(event.size()).index("("):]
+        # newEventWidth = int(newEventDimensions[:newEventDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
+        # newEventHeight = int(newEventDimensions[newEventDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
+
+        # self.centerWindow()
+
+    def wheelEvent(self, event):
+        print(event.angleDelta())
+
+    def mouseMoveEvent(self, event):
+        self.Frame.move(event.x(),event.y())
+
     def retranslateUi(self, MAIN):
         _translate = QtCore.QCoreApplication.translate
         MAIN.setWindowTitle(_translate("MAIN", "IMG Viewer"))
 
-    def centerWindow(self, MAIN):
-        rect = MAIN.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        rect.moveCenter(cp)
-        # MAIN.move(rect.topLeft())
+    def centerWindow(self):
+        fg = self.Img.frameGeometry()
+        cp = self.Frame.frameGeometry().center()
+        fg.moveCenter(cp)
+        self.Img.move(fg.topLeft())
+
 
     def resizeWindow(self, MAIN):
-
         imgDimensions = str(self.imgFile.size())[str(self.imgFile.size()).index("("):]
         imgWidth = int(imgDimensions[:imgDimensions.index(",")] .replace(",","").replace(" ","").replace("(",""))
         imgHeight = int(imgDimensions[imgDimensions.index(","):].replace(",","").replace(" ","").replace(")",""))
@@ -79,24 +107,22 @@ class Ui_MAIN(object):
     
 
 
-        MAIN.setMinimumSize(self.imgFile.width(),self.imgFile.height())
+        # MAIN.setMinimumSize(self.imgFile.width(),self.imgFile.height())
+        MAIN.setGeometry(0,0,self.sysWidth,self.sysHeight)
         MAIN.showMaximized()
+        self.Frame.setGeometry(0,0,MAIN.width(),MAIN.height())
         self.Img.setPixmap(self.imgFile)
-        rect = self.Frame.frameGeometry()
-        cp = MAIN.frameGeometry().center()
-        rect.moveCenter(cp)
-        self.Frame.move(rect.topLeft())
-
         MAIN.setWindowTitle(fileName)
-        # MAIN.resize(self.sysWidth,self.sysHeight)
-        self.Frame.setGeometry(0,0,imgWidth,self.sysHeight)
         self.Img.adjustSize()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MAIN = QtWidgets.QMainWindow()
-    ui = Ui_MAIN()
-    ui.init(MAIN)
-    MAIN.show()
+    MAIN = MainWindow(QtWidgets.QMainWindow())
+
+    # ui = MainWindow(MAIN)
+    # ui.show()
+    # ui.__init__(MAIN)
+
+    MAIN.init(MAIN)
     sys.exit(app.exec_())
